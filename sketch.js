8f7965895ec0058e119e2f44c45043a2d9ff6803
Phenicien <98313230+Phenicien@users.noreply.github.com>
2022-01-28 15:45:26 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
+let x,y,r,g,b;
 function setup() {
-  createCanvas(400, 300);
-    background(0);
+  createCanvas(windowWidth,windowHeight);
+  background(0)
 }
 
 function draw() {
-  noStroke();
-  fill(400, 300, 100)
-
-  circle(mouseX, mouseY, 24);
+  
+  r = random(0,255);
+  g = 0;
+  b = random(0,300);
+  x = random(0,600);
+  y = random(0,400);
+  // noStroke();
+  fill(r,g,b,2000);
+  circle(x,y,100);
+  ellipse(x,y,100);
+  square(x,y,100);
+  triangle(x,y,b,100,200,300);
   
 }
-function mousePressed(){
-  background(0);
-}
+
