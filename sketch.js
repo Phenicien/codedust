7acd277e171c5f068c2b64c07bb2799f9d6ff46a
Phenicien <98313230+Phenicien@users.noreply.github.com>
2022-01-28 15:23:17 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(200, 150,200);
+  quad(38, 31, 86, 20, 69, 63, 30, 76);
+  ellipse(10,010,10)
+  fill(100,300,500)
+line(0,50,400,300)
+}
+ 
+  
+  
+  
