65d6e036562df8201e34b80f48ea01180aa9fedd
Phenicien <98313230+Phenicien@users.noreply.github.com>
2022-02-04 15:29:35 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(100);
-  stroke(255);
-  strokeWeight(4)
-  noFill();
-  
-  if (mouseX < 300){
-    fill(255,100,100);
+  background(220);
+  if(mouseIsPressed){
+    background(0,255,0);
   }
-  ellipse(200,200,100,100);
+  stroke(255);
+  strokeWeight(4);
+  fill(200,100,100);
+  if(mouseX>250&& mouseX <350&& mouseX){
+  fill(25,0,200)
+  }
+  rectMode(CENTER);
+  rect(300,200,100,100)
+ fill(25,0,200);
+  
+   ellipse(200,200,100,100,100);
+  
 }
