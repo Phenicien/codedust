7d7817abe80715bdffb512c87b9b7807fb9d76ca
Phenicien <98313230+Phenicien@users.noreply.github.com>
2022-02-04 14:45:21 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,15 @@
-// Code example
-// Bay Panique
-// This coding train
-//Learn from Laurnen 
 function setup() {
-  createCanvas(400, 300);
-  print("Hello dear web developers")
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(100);
-  
- 
- rectMode(CENTER);
-  // this is the green rectangle in the center
-  fill(0,0,255);
+  stroke(255);
   strokeWeight(4)
-  stroke(0,255,0);
- rect(200,150,150,150);
-  //Red circle 
-  //Late i want make this circle animate
-  fill(255,0,0,100)
-  noStroke(255)
-  ellipse(150,250,100,75)
+  noFill();
   
-    
+  if (mouseX < 300){
+    fill(255,100,100);
+  }
+  ellipse(200,200,100,100);
 }
